afa125218f714aefafaae12700b340e3e31271c4
AlyanQureshi <144194260+AlyanQureshi@users.noreply.github.com>
2024-02-15 20:44:39 -0700

    Adding Family Relation.java file

---
+++ FamilyRelation.java
@@ -0,0 +1,40 @@
+package edu.ucalgary.oop;
+
+public Class FamilyRelation {
+    private DisasterVictim personOne;
+    private String relationshipTo;
+    private DisasterVictim personTwo;
+
+    // Constructor:
+    public FamilyRelation(DisasterVictim personOne, String relationshipTo, DisasterVictim personTwo) {
+        this.personOne = personOne;
+        this.relationshipTo = relationshipTo;
+        this.personTwo = personTwo;
+    }
+
+    // Getters:
+    public DisasterVictim getPersonOne() {
+        return personOne;
+    }
+
+    public String getRelationshipTo() {
+        return relationshipTo;
+    }
+
+    public DisasterVictim getPersonTwo() {
+        return personTwo;
+    }
+
+    // Setters: 
+    public void setPersonOne(DisasterVictim personOne) (
+        this.personOne = personOne;
+    )
+
+    public void setRelationshipTo(String relationshipTo) {
+        this.relationshipTo = relationshipTo;
+    }
+    
+    public void setPersonTwo(DisasterVictim personTwo) {
+        this.personTwo = personTwo;
+    }
+}
