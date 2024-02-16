1df055d6fe0c81583cb855c2494492d387b5d5fe
AlyanQureshi <144194260+AlyanQureshi@users.noreply.github.com>
2024-02-15 22:04:02 -0700

    Adding Medical Record file with exception handling

---
+++ MedicalRecord.java
@@ -0,0 +1,48 @@
+package edu.ucalgary.oop;
+
+public Class MedicalRecord {
+    private Location location;
+    private String treatmentDetails;
+    private String dateOfTreatment;
+
+    // Contructor:
+    public MedicalRecord(Location location, String treatmentDetails, String dateOfTreatment) throws IllegalArgumentException{
+        String regex = "\\d{4}-\\d{2}-\\d{2}";
+        if (!dateOfTreatment.matches(regex)) {
+            throw new IllegalArgumentException(s:"Date Format Is Invalid. Expected: YYYY-MM-DD");
+        }
+        this.location = location;
+        this.treatmentDetails = treatmentDetails;
+        this.dateOfTreatment = dateOfTreatment;
+    }
+
+    // Getters:
+    public Location getLocation() {
+        return location;
+    }
+
+    public String getTreatmentDetails() {
+        return treatmentDetails;
+    }
+
+    public String getDateOfTreatment() {
+        return dateOfTreatment;
+    }
+
+    // Setters:
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public void setTreatmentDetails(String treatmentDetails) {
+        this.treatmentDetails = treatmentDetails;
+    }
+
+    public void setDateOfTreatment(String dateOfTreatment) throws IllegalArgumentException{
+        String regex = "\\d{4}-\\d{2}-\\d{2}";
+        if (!dateOfTreatment.matches(regex)) {
+            throw new IllegalArgumentException(s:"Date Format Is Invalid. Expected: YYYY-MM-DD");
+        }
+        this.dateOfTreatment = dateOfTreatment;
+    }
+}
